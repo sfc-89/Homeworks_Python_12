6f23b9770b44a284f88bb469ed31823748b15e74
sfc-89 <sfc89@onionmail.org>
2026-01-22 12:32:57 +0200

    Initial commit.

---
+++ HW_18_Yura_Fedotov/HW_18_Yura_Fedotov.py
@@ -0,0 +1,25 @@
+import random
+
+
+class Budget:
+    pass
+
+
+class WorkerHour(Budget):
+    pass
+
+
+class WorkerFixed(Budget):
+    pass
+
+
+class WorkerFOP(Budget):
+    pass
+
+
+class WorkerSelf(Budget):
+    pass
+
+
+if __name__ == '__main__':
+    pass
