ebf439d1844414aef45a5c70126cc06c5c33a55c
sfc-89 <sfc89@onionmail.org>
2026-01-22 14:51:45 +0200

    Added params of classes.

--- HW_18_Yura_Fedotov/HW_18_Yura_Fedotov.py
+++ HW_18_Yura_Fedotov/HW_18_Yura_Fedotov.py
@@ -1,24 +1,84 @@
 import random
 
-
-class Budget:
-    pass
+from abc import ABC, abstractmethod
 
 
-class WorkerHour(Budget):
-    pass
+class ItWorker(ABC):
+    def __init__(self):
+        self.salary = self.calculate_salary()
+        self.WAR_TAX = 0.015
+        self.PDFO = 0.18
+        self.EP = 0.05
+        self.ESV = 704
+        self.calculate_tax()
+
+    @abstractmethod
+    def calculate_salary(self):
+        return self.salary * 1
+
+    @abstractmethod
+    def calculate_tax(self):
+        self.salary = self.salary - (
+                self.PDFO * self.salary + self.WAR_TAX * self.salary
+        )
+
+    @staticmethod
+    def sort_workers(*workers: object):
+        return sorted(workers, key=lambda worker: worker.salary)
 
 
-class WorkerFixed(Budget):
-    pass
+class WorkerHour(ItWorker):
+    def __init__(self, hours:float, hour_salary:float):
+        self.hours = hours
+        self.hour_salary = hour_salary
+        super().__init__()
+
+    def calculate_salary(self):
+        return self.hours * self.hour_salary
+
+    def calculate_tax(self):
+        super().calculate_tax()
 
 
-class WorkerFOP(Budget):
-    pass
+class WorkerFixed(ItWorker):
+    def __init__(self, fixed_salary: float):
+        self.salary = fixed_salary
+        super().__init__()
+
+    def calculate_salary(self):
+        return super().calculate_salary()
+
+    def calculate_tax(self):
+        super().calculate_tax()
 
 
-class WorkerSelf(Budget):
-    pass
+class WorkerFOP(ItWorker):
+    def __init__(self, hours: float, hour_salary: float):
+        self.hours = hours
+        self.hour_salary = hour_salary
+        super().__init__()
+
+    def calculate_salary(self):
+        return self.hours * self.hour_salary * 1.1
+
+    def calculate_tax(self):
+        self.salary = self.salary - (
+                self.EP * self.salary + self.ESV
+        )
+
+class WorkerSelf(ItWorker):
+    def __init__(self, lines: float, lines_salary: float):
+        self.lines = lines
+        self.lines_salary = lines_salary
+        super().__init__()
+
+    def calculate_salary(self):
+        return self.lines * self.lines_salary
+
+    def calculate_tax(self):
+        self.salary = self.salary - (
+                self.PDFO * self.salary + self.WAR_TAX * self.salary + self.ESV
+        )
 
 
 if __name__ == '__main__':
