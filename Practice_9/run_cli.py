db3e7bd72c38d30c502ad0eedf8ceac0434c7fed
sfc-89 <sfc89@onionmail.org>
2026-02-04 14:26:28 +0200

    Initial commit.

---
+++ Practice_9/run_cli.py
@@ -0,0 +1,22 @@
+
+
+# card
+
+# hand
+#   - cards = []
+#   - prop ->  points
+#   - m -> add_card
+#   - m -> str
+
+# deck
+#   - deck
+#   - m -> shuffle
+#   - m ->  get_card()
+
+# players
+#  abs_player
+#
+#   bots ???
+#   dealer
+
+#   player
