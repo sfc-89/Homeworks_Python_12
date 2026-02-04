1cbbc8dbb944446c7f27a69b871ccc3aa09958e4
sfc-89 <sfc89@onionmail.org>
2026-02-04 15:54:41 +0200

    Added name, sleep(0.5) in play_round.

--- Practice_9/game.py
+++ Practice_9/game.py
@@ -2,18 +2,20 @@ if __name__ != '__main__':
     from deck import Deck
     from players import Player, Dealer
 
+from time import sleep
+
 
 class CLI_Game:
 
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name
         self.deck = Deck(1)
 
     def init(self):
-        self.player = Player(name="My Name")
+        self.player = Player(name=self.name)
         self.dealer = Dealer()
 
     def check_score(self):
-        # TODO
         if self.player.full_points == self.dealer.full_points and self.player.full_points < 21:
             return {"winner": "draw"}
         elif self.dealer.full_points < self.player.full_points <= 21 and self.dealer.full_points < 21:
@@ -36,19 +38,15 @@ class CLI_Game:
         self.dealer.take_two_cards([self.deck.get_card(), self.deck.get_card()])
         self.player.take_two_cards([self.deck.get_card(), self.deck.get_card()])
 
-        print(self.dealer)
-        print(self.player)
-
         self.check_score()
 
     def play_round(self, action):
         if action:
             self.player.take_card(self.deck.get_card())
-            print(self.player)
         else:
             while self.dealer.full_points <= self.dealer.max_points:
+                sleep(0.5)
                 self.dealer.take_card(self.deck.get_card())
-                print(self.dealer)
 
     def play(self):
         self.init()
@@ -59,6 +57,7 @@ class CLI_Game:
             action = self.player.ask_card()
             self.play_round(action)
             is_going_on = self.is_going_on(action)
+
         return self.check_score()
 
 
@@ -66,5 +65,5 @@ if __name__ == '__main__':
     from deck import Deck
     from players import Player, Dealer
 
-    game = CLI_Game()
+    game = CLI_Game("Test")
     game.play()
