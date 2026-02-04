c34c5c5d5f8508708e0cf7855f59d93a824cd5a4
sfc-89 <sfc89@onionmail.org>
2026-02-04 15:10:24 +0200

    Fixed playing logic - added play_round, fixed is_going_on().

--- Practice_9/game.py
+++ Practice_9/game.py
@@ -13,12 +13,22 @@ class CLI_Game:
         self.player = Player(name="My Name")
         self.dealer = Dealer()
 
-    def is_going_on(self):
-        # !TODO
+    def check_score(self):
+        #TODO
+        if self.player.full_points == self.dealer.full_points and self.player.full_points < 21:
+            return {"winner":"draw"}
+        elif self.dealer.full_points < self.player.full_points <= 21 and self.dealer.full_points < 21:
+            return {"winner":self.player.name}
+        elif self.dealer.full_points > 21 >= self.player.full_points:
+            return {"winner": self.player.name}
+        else:
+            return {"winner": self.dealer.name}
+
+    def is_going_on(self, action):
+        if action == "2":
+            return False
         return True
 
-    def _check_ctate(self):
-        pass
 
     def start(self):
         self.deck.shuffle()
@@ -31,9 +41,20 @@ class CLI_Game:
         print(self.dealer)
         print(self.player)
 
-        self._check_ctate()
+        self.check_score()
 
-    def play_round(self):
+    def play_round(self, action):
+        if action == "1":
+            self.player.take_card(self.deck.get_card())
+            print(self.player)
+        else:
+            while self.dealer.full_points <= self.dealer.max_points:
+                self.dealer.take_card(self.deck.get_card())
+                print(self.dealer)
+
+
+        #elif self.dealer.full_points < 17:
+        #    self.dealer.take_card(self.deck.get_card())
         # !TODO
         pass
 
@@ -41,11 +62,13 @@ class CLI_Game:
     def play(self):
 
         self.init()
+        self.start()
+        is_going_on = True
 
-        while self.is_going_on():
-            #TODO: delete recursion
-            self.start()
-            self.play_round()
+        while is_going_on:
+            action = input("Press 1 to take card, 2 to stand.")
+            self.play_round(action)
+            is_going_on = self.is_going_on(action)
             # TODO: self.show_results()
 
 
