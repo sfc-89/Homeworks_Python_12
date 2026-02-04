bc3fb81a9832b457b01aacb90c2960fb212aa9f7
sfc-89 <sfc89@onionmail.org>
2026-02-04 15:31:12 +0200

    Changer play() return to check.score(), other small fixes.

--- Practice_9/game.py
+++ Practice_9/game.py
@@ -1,4 +1,3 @@
-
 if __name__ != '__main__':
     from deck import Deck
     from players import Player, Dealer
@@ -7,28 +6,27 @@ if __name__ != '__main__':
 class CLI_Game:
 
     def __init__(self):
-        self.deck = Deck(3)
+        self.deck = Deck(1)
 
     def init(self):
         self.player = Player(name="My Name")
         self.dealer = Dealer()
 
     def check_score(self):
-        #TODO
+        # TODO
         if self.player.full_points == self.dealer.full_points and self.player.full_points < 21:
-            return {"winner":"draw"}
+            return {"winner": "draw"}
         elif self.dealer.full_points < self.player.full_points <= 21 and self.dealer.full_points < 21:
-            return {"winner":self.player.name}
+            return {"winner": self.player.name}
         elif self.dealer.full_points > 21 >= self.player.full_points:
             return {"winner": self.player.name}
         else:
             return {"winner": self.dealer.name}
 
     def is_going_on(self, action):
-        if action == "2":
+        if not action:
             return False
         return True
-
 
     def start(self):
         self.deck.shuffle()
@@ -44,7 +42,7 @@ class CLI_Game:
         self.check_score()
 
     def play_round(self, action):
-        if action == "1":
+        if action:
             self.player.take_card(self.deck.get_card())
             print(self.player)
         else:
@@ -52,24 +50,16 @@ class CLI_Game:
                 self.dealer.take_card(self.deck.get_card())
                 print(self.dealer)
 
-
-        #elif self.dealer.full_points < 17:
-        #    self.dealer.take_card(self.deck.get_card())
-        # !TODO
-        pass
-
-
     def play(self):
-
         self.init()
         self.start()
         is_going_on = True
 
         while is_going_on:
-            action = input("Press 1 to take card, 2 to stand.")
+            action = self.player.ask_card()
             self.play_round(action)
             is_going_on = self.is_going_on(action)
-            # TODO: self.show_results()
+        return self.check_score()
 
 
 if __name__ == '__main__':
@@ -78,5 +68,3 @@ if __name__ == '__main__':
 
     game = CLI_Game()
     game.play()
-
-
