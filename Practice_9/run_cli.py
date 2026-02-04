cf88c8f2bf56c3b839035cb1c715b1bb0d178c54
sfc-89 <sfc89@onionmail.org>
2026-02-04 15:39:44 +0200

    Added game interface.

--- Practice_9/run_cli.py
+++ Practice_9/run_cli.py
@@ -1,4 +1,15 @@
+import game as api
 
+
+if __name__ == '__main__':
+    action = input("Welcome to the BlackJack Redux Game!\n1 - to play, 2 - to exit: ")
+    while action == "1":
+        game = api.CLI_Game()
+        result = game.play()
+        if result["winner"] == "draw":
+            action = input(f"Draw! \nPress 1 to play new round, 2 to exit: ")
+        else:
+            action = input(f"Winners is {result['winner']}! \nPress 1 to play new round, 2 to exit: ")
 
 # card
 
