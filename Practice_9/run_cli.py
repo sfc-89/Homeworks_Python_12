4f25aeae5532de9a99b7eea4b8d57d7ed3bab551
sfc-89 <sfc89@onionmail.org>
2026-02-04 15:55:28 +0200

    Added name, \n to print()

--- Practice_9/run_cli.py
+++ Practice_9/run_cli.py
@@ -1,33 +1,12 @@
 import game as api
 
-
 if __name__ == '__main__':
-    action = input("Welcome to the BlackJack Redux Game!\n1 - to play, 2 - to exit: ")
+    name = input("Welcome to the BlackJack Redux Game!\nWhat is your name?:")
+    action = input("\n1 - to play, 2 - to exit: ")
     while action == "1":
-        game = api.CLI_Game()
+        game = api.CLI_Game(name)
         result = game.play()
         if result["winner"] == "draw":
-            action = input(f"Draw! \nPress 1 to play new round, 2 to exit: ")
+            action = input(f"\nDraw! \n\nPress 1 to play new round, 2 to exit: ")
         else:
-            action = input(f"Winners is {result['winner']}! \nPress 1 to play new round, 2 to exit: ")
-
-# card
-
-# hand
-#   - cards = []
-#   - prop ->  points
-#   - m -> add_card
-#   - m -> str
-
-# deck
-#   - deck
-#   - m -> shuffle
-#   - m ->  get_card()
-
-# players
-#  abs_player
-#
-#   bots ???
-#   dealer
-
-#   player
+            action = input(f"\nWinner is {result['winner']}! \n\nPress 1 to play new round, 2 to exit: ")
